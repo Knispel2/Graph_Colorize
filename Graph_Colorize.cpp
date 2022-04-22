6c84be3788e26567b610e3b5edeeeafcf2458106
Timofei Pshenichniy <timofei.psheno@gmail.com>
2022-04-22 23:00:34 +0300

    Считывание из файла + Загрузка тестов + Загрузка графа в память

--- Graph_Colorize.cpp
+++ Graph_Colorize.cpp
@@ -5,6 +5,16 @@
 #include <algorithm>
 using namespace std;
 
+class edge
+{
+public:
+    int x, y;
+    edge(int a, int b) : x(a), y(b)
+    {
+
+    };
+};
+
 vector <string> list_files(string dir)
 {
     vector <string> result;
@@ -13,18 +23,49 @@ vector <string> list_files(string dir)
     return result;
 }
 
-Cord split(string& data, string file_debug = "")
+edge split(string& data, string file_debug = "")
 {
     auto pos = data.find(" ");
     int transp;
     if (data.find("  ") != string::npos) transp = 2;
     else transp = 1;
-    return Cord(stod(data.substr(0, pos)), stod(data.substr(pos + transp)));
+    return edge(stod(data.substr(0, pos)), stod(data.substr(pos + transp)));
 }
 
 
 int main()
 {
-
+    vector <string> data = list_files("data");
+    string buf;
+    ofstream fout;
+    fout.open("result.txt");
+    string x;
+    try {
+        for (int k = 0; k < data.size(); k++)
+        {
+            x = data[k];
+            cout << "Starting " << x << endl << flush;
+            ifstream file("data/" + x);
+            getline(file, buf);
+            edge start_data = split(buf);
+            int N = start_data.x;
+            vector <vector <int>> Graph (N, vector <int> (0))
+            while (getline(file, buf))
+            {
+                if (buf == "") continue;
+                start_data = split(buf);
+                Graph[start_data.x].push_back(start_data.y);
+                Graph[start_data.y].push_back(start_data.x);
+            }
+            file.close();
+            
+        }
+    }
+    catch (exception& e)
+    {
+        cout << e.what();
+        cin.get();
+    }
+    fout.close();
 }
 
