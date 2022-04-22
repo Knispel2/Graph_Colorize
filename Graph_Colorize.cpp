4d645fc0a44914bde19d783eb5db10d83fa81bc2
Timofei Pshenichniy <timofei.psheno@gmail.com>
2022-04-22 22:11:17 +0300

    Добавьте файлы проекта.

---
+++ Graph_Colorize.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+vector <string> list_files(string dir)
+{
+    vector <string> result;
+    for (const auto& entry : filesystem::directory_iterator(dir))
+        result.push_back(entry.path().string().substr(5));
+    return result;
+}
+
+Cord split(string& data, string file_debug = "")
+{
+    auto pos = data.find(" ");
+    int transp;
+    if (data.find("  ") != string::npos) transp = 2;
+    else transp = 1;
+    return Cord(stod(data.substr(0, pos)), stod(data.substr(pos + transp)));
+}
+
+
+int main()
+{
+
+}
+
